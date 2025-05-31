c303bfdcf09f989e809dd85a91decf08b1c7f2bd
DBricksEY <george.matzavinos@gr.ey.com>
2025-05-31 19:30:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
+require("dotenv").config(); // Load environment variables from .env
 
 const app = express();
 const PORT = process.env.PORT || 3000;
